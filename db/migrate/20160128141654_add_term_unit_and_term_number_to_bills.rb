716ba55250627619332ec01e5edf8d014f878cde
John Setiawan <jsetiawan@capterra.com>
2016-01-28 09:26:52 -0500

    Add comment.

--- db/migrate/20160128141654_add_term_unit_and_term_number_to_bills.rb
+++ db/migrate/20160128141654_add_term_unit_and_term_number_to_bills.rb
@@ -1,3 +1,4 @@
+# rails g migration AddTermUnitAndTermNumberToBills term_unit:integer term_number:integer
 class AddTermUnitAndTermNumberToBills < ActiveRecord::Migration
   def change
     add_column :bills, :term_unit, :integer
