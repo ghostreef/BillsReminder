d6c9f7243e41ff40e62f19dd4244c522ab8cbbbc
John Setiawan <jsetiawan@capterra.com>
2016-02-18 13:34:45 -0500

    Add association between purpose and source.

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -2,6 +2,8 @@
 class Purpose < ActiveRecord::Base
   validates :name, uniqueness: true, length: { minimum: 1 }
 
+  has_many :sources
+
   def custom_error_messages
     errors.map do |attribute, error|
       "'#{send(attribute)}' #{error}."
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,4 +1,8 @@
 class Source < ActiveRecord::Base
   validates :name, :regex, presence: true, length: { minimum: 1 }
   validates :name, uniqueness: true
+
+  # in my head this is backwards, but the fk goes in this table
+  belongs_to :purpose
+  # also note if a purpose is deleted, purpose_id remains, but .purpose will return nil
 end
