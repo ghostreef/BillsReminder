23dbfdd6c83a9a40467cc6d367684e2c98eb9aa2
John Setiawan <jsetiawan@capterra.com>
2016-02-21 12:20:59 -0500

    Add purpose association to category.

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ActiveRecord::Base
   validates :name, uniqueness: true, length: { minimum: 1 }
 
   has_and_belongs_to_many :sources
+  has_and_belongs_to_many :purposes
 
   def transactions
 
--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -3,6 +3,7 @@ class Purpose < ActiveRecord::Base
   validates :name, uniqueness: true, length: { minimum: 1 }
 
   has_many :sources
+  has_and_belongs_to_many :categories
 
   def custom_error_messages
     errors.map do |attribute, error|
