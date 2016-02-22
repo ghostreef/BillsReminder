c076ea7e5c2fe59bb95ac71741165b620f39e0ee
John Setiawan <jsetiawan@capterra.com>
2016-02-22 11:48:25 -0500

    Add highlighting to row. Add move over capitalize name.

--- app/assets/javascripts/sources.js
+++ app/assets/javascripts/sources.js
@@ -15,4 +15,16 @@ $(document).ready(function () {
         $('input, select', $fieldset).val('');
         $(this).parent().append($fieldset);
     });
+
+    $('.js-fill_in_name').on('click', function(event) {
+        var regex = $(this).siblings('.js-regex').val().split(' ');
+
+        for(var i = 0; i < regex.length; i++) {
+            regex[i] = regex[i][0].toUpperCase() + regex[i].substr(1).toLowerCase();
+        }
+
+        regex = regex.join(' ');
+
+        $(this).siblings('.js-name').val(regex);
+    });
 });
