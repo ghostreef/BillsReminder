07dcb1b7e3a04937ed63fd32aa9e8dbeea39b963
John Setiawan <jsetiawan@capterra.com>
2016-02-27 12:20:21 -0500

    Update parser views.

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -8,4 +8,24 @@ class Parser < ActiveRecord::Base
        }
 
   has_and_belongs_to_many :transformations
+
+  def date_transformations
+    transformations.where(transformation_type: 0)
+  end
+
+  def split_transformations
+    transformations.where(transformation_type: 1)
+  end
+
+  def other_transformations
+    transformations.where(transformation_type: [2, 3])
+  end
+
+  # only one parser can be enabled at a time
+  def self.enable(parser)
+    # disable all parses
+    Parser.where(status: Parser.statuses[:enabled]).update_all(status: Parser.statuses[:disabled])
+
+    parser.update(status: Parser.statuses[:enabled])
+  end
 end
