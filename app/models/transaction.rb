d13130ce8961ffc918be389b0a81352567a4be63
John Setiawan <jsetiawan@capterra.com>
2016-03-02 19:19:22 -0500

    Fix guess_purpose, add split_description.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -21,13 +21,31 @@ class Transaction < ActiveRecord::Base
   # Feels wrong that guess_purpose relies on guess_source, but purpose comes from source.
   def guess_purpose
     # use most recent source guess, if none available make the guess, the guess may still be nil
-    (@source || source || guess_source).try(default_purpose)
+    (@source || source || guess_source).try(:default_purpose)
+  end
+
+
+  # an EXPERIMENT, purpose is derived from source and they are both object oriented
+  # similarly locality is derived from region, but they are both in the generic object transformations
+  # really source and purpose could also transformations, but they loose the association
+
+  def guess_region
+
+  end
+
+  def guess_locality
+
+  end
+
+  def split_description
+    regexp = Regexp.new(Parser.parser.split_transformation.pattern)
+    raw_description.split(regexp).map(&:strip).delete_if(&:empty?)
   end
 
   def generate_description
 
   end
-
+  
   # faster than the first try, but could still be better
   def self.find_pattern_in_words(patterns, words, case_insensitive)
 
