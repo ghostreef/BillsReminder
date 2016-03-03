97c496831efa8844eb2bc411967f0550f31f9ce0
John Setiawan <jsetiawan@capterra.com>
2016-03-03 18:24:40 -0500

    Update transformations model and controller.

--- app/models/transformation.rb
+++ app/models/transformation.rb
@@ -1,3 +1,36 @@
 class Transformation < ActiveRecord::Base
+  # regex, case_insensitive, derives, value, implies, set, complexity
+  validates :regex, uniqueness: true, length: {minimum: 1}
+
+  belongs_to :transformation
+  alias_attribute :implies, :transformation
+
+  before_create :set_default_values
+
+
+  # these give us Transformation.date, Transformation.split...
+  enum set: {
+           date: 0,
+           split: 1,
+           transform: 2,
+           strip: 3
+       }
+
+  enum complexity: {
+           simple: 1,
+           straightforward: 2,
+           complex: 3
+       }
+
+  def custom_error_messages
+
+  end
+
+  private
+
+  def set_default_values
+    self.value ||= ''
+    self.complexity = (self.regex =~ /\*\[\]\(\)\+\\\$\^\?\{\}\.\|/) ? 3 : 1
+  end
 
 end
