b16e9a381e4ce5a353f430372043c754f27599eb
John Setiawan <jsetiawan@capterra.com>
2016-03-20 21:03:36 -0400

    Don't need this anymore.

--- app/assets/javascripts/parsers.js
+++ app/assets/javascripts/parsers.js
@@ -1,9 +1,3 @@
 $(function() {
-    $('#js-search_order_1, #js-search_order_2').sortable({
-        connectWith: ".js-search_connected_sortable"
-    }).disableSelection();
-
-    $('#js-expected_order_1, #js-expected_order_2').sortable({
-        connectWith: ".js-expected_connected_sortable"
-    }).disableSelection();
+    $('.js-sortable').sortable().disableSelection();
 });
