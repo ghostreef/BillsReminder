3b84434d3eed44d6010ace52a5cc63dfa8f78665
John Setiawan <jsetiawan@capterra.com>
2016-03-20 21:03:09 -0400

    Update parser form. Add my own multi select, turns out to be super simple. Will probably delete the plugin.

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-    $('.js-multiSelect').multiSelect()
+    $('.js-multiSelect').multiSelect();
+
+    $('.js-better-select li').on('click', function(event) {
+        $(this).parent().siblings().append(this);
+        $('input', this).attr('disabled', function(i, v) { return !v; })
+    });
 });
+
