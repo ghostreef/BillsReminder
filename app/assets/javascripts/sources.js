2c3511610ca9a5b5f62575a5934b822d4dcdbde3
John Setiawan <jsetiawan@capterra.com>
2016-04-07 22:27:40 -0400

    Maybe add a total range to bubbles.

--- app/assets/javascripts/sources.js
+++ app/assets/javascripts/sources.js
@@ -23,6 +23,21 @@ $(document).ready(function () {
     $('.js-bubble').each(function() {
         $(this).css('background-color',getRandomColor());
     });
+
+
+        $("#slider-range").slider({
+            range: true,
+            min: 0,
+            max: 500,
+            values: [75, 300],
+            slide: function (event, ui) {
+                $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+            }
+        });
+
+        $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+        " - $" + $("#slider-range").slider("values", 1));
+
 });
 
 // copy text from regex field and move over to name field
