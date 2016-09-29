7af8065d0b92ae9aed510a6c8d66afb4a312cfae
John Setiawan <jsetiawan@capterra.com>
2016-09-29 08:02:15 -0400

    Add basic html authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :helpful_tips
+  http_basic_authenticate_with name: Rails.application.secrets.username, password: Rails.application.secrets.password
+
   def dashboard
 
   end
